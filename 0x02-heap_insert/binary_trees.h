512a84769e1c1a3bc149834236b5dd1c4165fb23
bryano13 <bryano13@hotmail.com>
2021-02-11 22:51:02 -0500

    .h

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,8 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -12,37 +12,24 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
-typedef struct binary_tree_s binary_tree_t;
-
-/**
- * struct empty_space_s - Struct to find the empty space
- *
- * @deep: Deep of the node
- * @node: Node to find place
- */
-struct empty_space_s
-{
-    int deep;
-    heap_t *node;
-};
-
-typedef struct empty_space_s empty_space_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
 
-heap_t *free_space(heap_t *parent);
-heap_t *swap_nodes(heap_t *node);
-empty_space_t find_space(heap_t *node);
+void heap_max(heap_t **pa, heap_t **nw, size_t ht, size_t sz,
+              size_t mi, size_t comp);
+size_t sizeHeightMiMxLv(heap_t *tree, size_t sh, size_t height);
+size_t powR(size_t x, size_t y);
+void changeParent(heap_t **new);
 
 #endif /* _BINARY_TREES_H_ */
